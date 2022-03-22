61fee5cf4fe60b04e678002d02d5e88a74a25597
Sobhie <sobhie.saad1@gmail.com>
2022-03-22 13:08:32 +0300

    initialize navbar

---
+++ views/login.php
@@ -0,0 +1,16 @@
+<?php
+   include("config.php");
+   include("index.php");
+
+?>
+<form action="authenticate.php" method="post">
+    <label for="username">
+        <i class="fas fa-user"></i>
+    </label>
+    <input type="text" name="username" placeholder="Username" id="username" required>
+    <label for="password">
+        <i class="fas fa-lock"></i>
+    </label>
+    <input type="password" name="password" placeholder="Password" id="password" required>
+    <input type="submit" value="Login">
+</form>
